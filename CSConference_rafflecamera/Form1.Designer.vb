48b2a80e5c818bd811cab1759c9e536cb9d79b8e
Jose Miranda <feu.joseiii@gmail.com>
2017-06-26 18:11:47 +0800

    Initial Commit

---
+++ CSConference_rafflecamera/Form1.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button4 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 28)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(723, 402)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(741, 28)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(511, 342)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(882, 376)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(135, 54)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Take Picture"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(1143, 376)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(109, 54)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Retake Picture"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 436)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(1240, 233)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Logs"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(6, 19)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(1228, 208)
+        Me.TextBox2.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(741, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Current Task"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(1023, 376)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(114, 54)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Save"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(741, 376)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(135, 54)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "Check Task"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1264, 681)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button4 As Button
+End Class
---
+++ CSConference_rafflecamera/Form1.vb
@@ -0,0 +1,94 @@
+﻿Imports System.IO
+Imports System.Net
+Imports Emgu
+Imports Newtonsoft.Json
+
+Public Class Form1
+
+    Dim hasCaptured As Boolean = False
+    Dim camera As Emgu.CV.Capture
+    Dim imageHolder, capturedHolder As Bitmap
+    Dim apifindclient As New WebClient
+    Dim apidetailsfinder As New WebClient
+    Dim apiresponse As String
+
+    Dim jss As New Newtonsoft.Json.JsonSerializer
+
+    Dim apithread As Threading.Thread
+
+    Dim attendee_id As String
+
+    Dim apideleteclient As New WebClient
+    Dim apideletestring As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+        camera = New CV.Capture(1)
+    End Sub
+
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If hasCaptured Then
+            apideletestring = apideleteclient.DownloadString("http://localhost/feutechcs/public/deletependingtask/" + attendee_id)
+            capturedHolder.Save("raffle_images/" + attendee_id + ".jpg")
+            MsgBox("saved")
+            hasCaptured = False
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        hasCaptured = False
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        If hasCaptured = False Then
+            imageHolder = New Bitmap(camera.QueryFrame.Bitmap)
+            If imageHolder IsNot Nothing Then
+                PictureBox1.Image = imageHolder
+            End If
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        TextBox1.Text = ""
+        apiresponse = apifindclient.DownloadString("http://localhost/feutechcs/public/getpendingtask")
+        If apiresponse Is Nothing Or apiresponse Is String.Empty Then
+            MsgBox("no task")
+        Else
+            'apiresponse = attendee_id
+            Dim findDictionary As Dictionary(Of String, String) = jss.Deserialize(
+                Of Dictionary(Of String, String))(
+                New JsonTextReader(New StringReader(apiresponse)))
+
+            attendee_id = findDictionary("attendee_id")
+            Dim detailsurl As String = "http://localhost/feutechcs/public/getdetails1/" + attendee_id
+
+            Dim attendee_details As String = apidetailsfinder.DownloadString(detailsurl)
+
+            Dim detailsDictionary As Dictionary(Of String, String) = jss.Deserialize(
+                Of Dictionary(Of String, String))(
+                New JsonTextReader(New StringReader(attendee_details)))
+
+            Dim fullname As String = detailsDictionary("last_name") + ", " +
+                                    detailsDictionary("first_name") + " " +
+                                    detailsDictionary("middle_name").ElementAt(0) + "."
+            TextBox1.AppendText("Name: " + fullname + Environment.NewLine)
+            TextBox1.AppendText("Course: " + detailsDictionary("course") + Environment.NewLine)
+            TextBox1.AppendText("School: " + detailsDictionary("school") + Environment.NewLine)
+            TextBox1.AppendText("Email: " + detailsDictionary("email") + Environment.NewLine)
+            TextBox1.AppendText("Contact: " + detailsDictionary("contact") + Environment.NewLine)
+            TextBox1.AppendText("Date Registered: " + detailsDictionary("created_at") + Environment.NewLine)
+
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        capturedHolder = imageHolder
+        PictureBox1.Image = capturedHolder
+        hasCaptured = True
+    End Sub
+End Class
